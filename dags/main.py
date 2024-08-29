1b911b9a88a599c876955a61cfb06e619b4257c4
Hamdan Fadhillah <hamdanfadh@gmail.com>
2024-08-30 01:36:30 +0700

    update dag

--- dags/main.py
+++ dags/main.py
@@ -1,8 +1,21 @@
 from datetime import datetime
-from airflow import DAG
+from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
+import requests
 
-DAG(
-    dag_id = 'ingestion',
-    schedule= '* * * 5',
-)
+with DAG(dag_id = 'ingestion',
+         start_date=datetime(2022, 1, 1),
+         schedule="0 0 * * *"
+         ) as dag:
+    # Tasks are represented as operators
+
+    def extract(**kwargs):
+        req_extract = requests.get(url = 'https://randomuser.me/api/')
+        print(req_extract.json())
+
+    extract_task = PythonOperator(
+        task_id="extract",
+        python_callable=extract,
+    )
+
+    extract_task
